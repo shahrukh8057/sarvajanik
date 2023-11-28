22d2fe6b6bfc1274a71ab3d4a2c8314518ae5e19
Abhishek <singhchauhanabhishek816@gmail.com>
2023-11-28 14:36:37 +0530

    first commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB = "mongodb+srv://singhchauhanabhishek816:Abhi8057@cluster0.jsrqyqb.mongodb.net/imt-web-results?retryWrites=true&w=majority"
+const DB = "mongodb+srv://abhishekit365:Sunny123@jayramiti.a9oaeji.mongodb.net/JayRamIti?retryWrites=true&w=majority"
 
 mongoose.connect(DB).then(()=>{
     console.log("DataBase Connection SuccessFull")
@@ -13,6 +13,5 @@ db.on("error",console.log.bind(console,"Error in Connecting Database"));
 db.once("open",function(){
     console.log("SuccesFull Connected To the Database")
 })
-
 
 module.exports = db;
